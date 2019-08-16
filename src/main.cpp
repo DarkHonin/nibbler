db1f744757f4c13691b23ff9906b1a6291e0b390
William GOURLEY <wgourley@c4r24s3.wethinkcode.co.za>
2019-08-16 15:47:40 +0200

    fixed size args

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,9 @@ int main(int ac, char** av){
     int x = 20, y = 20;
     if (ac == 3){
         x = std::atoi(av[1]);
+        x = (x >= 20 ? x: 20);
         y = std::atoi(av[2]);
+        y = (y >= 20 ? y: 20);
     }
     Game game(x, y);
     game.run();
